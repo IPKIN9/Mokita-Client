30efc888cec99d70d8dfbc31a1d290855bbda614
ipkin9 <irwandirini@gmail.com>
2022-11-17 21:05:08 +0800

    Modify: Base select dafult value

--- src/components/input/BaseSelect.vue
+++ src/components/input/BaseSelect.vue
@@ -1,6 +1,7 @@
 <template>
   <select class="form-select" v-bind="$attrs" @change="eventTriger"
     @input="$emit('update:modelValue', $event.target.value)" :value="modelValue">
+    <option :value="null || ''" disabled>{{defaultVal ? defaultVal : '--Select Data--'}}</option>
     <option v-for="(obj, index) in options" :key="index" :value="obj[display.value]">{{ obj[display.label] }}</option>
   </select>
 </template>
@@ -16,6 +17,10 @@ const props = defineProps({
       value: 'id',
       label: 'name'
     }
+  },
+  defaultVal: {
+    type: String,
+    default: 'Select'
   },
   modelValue: [String, Number],
 })
